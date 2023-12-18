45bf5e337bdf26d9576024558d2e07fd7a5e6436
lisanna <olesia.kazakova.17@yandex.ru>
2023-12-18 20:30:17 +0500

    vse kategorii

--- one.py
+++ one.py
@@ -6,7 +6,7 @@ bot = telebot.TeleBot('6832822787:AAGaPrI_CiOLbI6HmDUjs-LOl4yjheMXVCk')
 
 @bot.message_handler(commands=['start'])
 def start(message):
-bot.send_message(message.chat.id, 'Привет! Чтобы начать игру напиши /game')
+bot.send_message(message.chat.id, 'Чтобы начать игру, напишите /game')
 
 @bot.message_handler(commands=['help'])
 def main(message):
@@ -17,30 +17,76 @@ def game(message):
 markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
 i1=types.KeyboardButton('мультфильмы')
 i2=types.KeyboardButton('фильмы')
-i3=types.KeyboardButton('сериалы')
-markup.add(i1, i2, i3)
+markup.add(i1, i2)
 bot.send_message(message.chat.id, 'Выберите раздел', reply_markup=markup)
 
 @bot.message_handler(content_types=['text'])
 def func(message):
 if message.text == 'фильмы':
+def ot15(message):
+if (message.text.lower() == 'пираты карибского моря'):
+bot.send_message(message.chat.id, 'Верно')
+else:
+bot.send_message(message.chat.id, 'Неверно. Правильный ответ: Пираты Карибского моря')
+bot.send_message(message.chat.id, 'Игра пройдена!')
+
+def ot14(message):
+if (message.text.lower() == 'тор') :
+bot.send_message(message.chat.id, 'Верно')
+else:
+bot.send_message(message.chat.id, 'Неверно. Правильный ответ: Тор')
+bot.send_message(message.chat.id, 'Далее: ')
+text = "\U00002620 \U0001F30A \U000026F5 \U0001F9AA \U000026AB"
+bot.send_message(message.chat.id, text=text)
+bot.register_next_step_handler(message, ot15)
+
+def ot13(message):
+if (message.text.lower() == 'хатико'):
+bot.send_message(message.chat.id, 'Верно')
+else:
+bot.send_message(message.chat.id, 'Неверно. Правильный ответ: Хатико')
+bot.send_message(message.chat.id, 'Далее: ')
+text = "\U0001F528 \U000026A1"
+bot.send_message(message.chat.id, text=text)
+bot.register_next_step_handler(message, ot14)
+
+def ot12(message):
+if (message.text.lower() == 'гарри поттер') or (message.text.lower() == 'гари потер') or (message.text.lower() == 'гарри потер') or (message.text.lower() == 'гари поттер'):
+bot.send_message(message.chat.id, 'Верно')
+else:
+bot.send_message(message.chat.id, 'Неверно. Правильный ответ: Гарри Поттер')
+bot.send_message(message.chat.id, 'Далее: ')
+text = "\U0001F686 \U0001F415"
+bot.send_message(message.chat.id, text=text)
+bot.register_next_step_handler(message, ot13)
+
+def ot11(message):
+if (message.text.lower() == 'капитан америка') or (message.text.lower() == 'первый мститель') :
+bot.send_message(message.chat.id, 'Верно')
+else:
+bot.send_message(message.chat.id, 'Неверно. Правильный ответ: Капитан Америка')
+bot.send_message(message.chat.id, 'Далее: ')
+text = "\U0001F9D1 \U0001F453 \U000026A1 \U0001FA84"
+bot.send_message(message.chat.id, text=text)
+bot.register_next_step_handler(message, ot12)
+
 def ot10(message):
 if (message.text.lower() == 'сумерки') :
 bot.send_message(message.chat.id, 'Верно')
 else:
 bot.send_message(message.chat.id, 'Неверно. Правильный ответ: Сумерки ')
-markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-i1 = types.KeyboardButton('мультфильмы')
-i2 = types.KeyboardButton('сериалы')
-markup.add(i1, i2)
-bot.send_message(message.chat.id, 'Хотите пройти другие разделы?', reply_markup=markup)
+bot.send_message(message.chat.id, 'Далее: ')
+text = "\U0001F6E1 \U0001F4AA \U0001F1FA\U0001F1F8"
+bot.send_message(message.chat.id, text=text)
+bot.register_next_step_handler(message, ot11)
 
 def ot9(message):
 if (message.text.lower() == 'гарфилд') or (message.text.lower() == 'garfield'):
 bot.send_message(message.chat.id, 'Верно')
 else:
 bot.send_message(message.chat.id, 'Неверно. Правильный ответ: Гарфилд')
-text = "Далее:\U0001F9DB \U00002764 \U0001F64D"
+bot.send_message(message.chat.id, 'Далее: ')
+text = "\U0001F9DB \U00002764 \U0001F469 "
 bot.send_message(message.chat.id, text=text)
 bot.register_next_step_handler(message, ot10)
 
@@ -49,7 +95,8 @@ if (message.text.lower() == 'планета обезьян'):
 bot.send_message(message.chat.id, 'Верно')
 else:
 bot.send_message(message.chat.id, 'Неверно. Правильный ответ: Планета обезьян')
-text = "Далее:\U0001F431 \U0001F4A4"
+bot.send_message(message.chat.id, 'Далее: ')
+text = "\U0001F431 \U0001F4A4"
 bot.send_message(message.chat.id, text=text)
 bot.register_next_step_handler(message, ot9)
 
@@ -58,7 +105,8 @@ if (message.text.lower() == 'трансформеры'):
 bot.send_message(message.chat.id, 'Верно')
 else:
 bot.send_message(message.chat.id, 'Неверно. Правильный ответ: Трансформеры')
-text = "Далее: \U0001F30E \U0001F412 \U0001F412 \U0001F412"
+bot.send_message(message.chat.id, 'Далее: ')
+text = "\U0001F30E \U0001F412 \U0001F412 \U0001F412"
 bot.send_message(message.chat.id, text=text)
 bot.register_next_step_handler(message, ot8)
 
@@ -67,7 +115,8 @@ if (message.text.lower() == 'оно'):
 bot.send_message(message.chat.id, 'Верно')
 else:
 bot.send_message(message.chat.id, 'Неверно. Правильный ответ: Оно')
-text = "Далее:\U0001F916 \U00002194 \U0001F697"
+bot.send_message(message.chat.id, 'Далее: ')
+text = "\U0001F916 \U00002194 \U0001F697"
 bot.send_message(message.chat.id, text=text)
 bot.register_next_step_handler(message, ot7)
 
@@ -76,7 +125,8 @@ if (message.text.lower() == 'титаник'):
 bot.send_message(message.chat.id, 'Верно')
 else:
 bot.send_message(message.chat.id, 'Неверно. Правильный ответ: Титаник')
-text = "Далее:\U0001F921 \U0001F388 \U0001F631"
+bot.send_message(message.chat.id, 'Далее: ')
+text = "\U0001F921 \U0001F388 \U0001F631"
 bot.send_message(message.chat.id, text=text)
 bot.register_next_step_handler(message, ot6)
 
@@ -85,7 +135,8 @@ if (message.text.lower() == 'маска') :
 bot.send_message(message.chat.id, 'Верно')
 else:
 bot.send_message(message.chat.id, 'Неверно. Правильный ответ: Маска')
-text = "Далее:\U0001F6E5 \U0001F9CA"
+bot.send_message(message.chat.id, 'Далее: ')
+text = "\U0001F6E5 \U0001F9CA"
 bot.send_message(message.chat.id, text=text)
 bot.register_next_step_handler(message, ot5)
 
@@ -94,7 +145,8 @@ if (message.text.lower() == 'бэтмэн') or (message.text.lower() == 'бет�
 bot.send_message(message.chat.id, 'Верно')
 else:
 bot.send_message(message.chat.id, 'Неверно. Правильный ответ: Бэтмэн')
-text = "Далее:\U0001F3AD \U0001F935"
+bot.send_message(message.chat.id, 'Далее: ')
+text = "\U0001F3AD \U0001F935"
 bot.send_message(message.chat.id, text=text)
 bot.register_next_step_handler(message, ot4)
 
@@ -103,7 +155,8 @@ if (message.text.lower() == 'призрачный гонщик') :
 bot.send_message(message.chat.id, 'Верно')
 else :
 bot.send_message(message.chat.id, 'Неверно. Правильный ответ: Призрачный гонщик ')
-text = "Далее:\U0001F935 \U0001F987"
+bot.send_message(message.chat.id, 'Далее: ')
+text = "\U0001F935 \U0001F987"
 bot.send_message(message.chat.id, text=text)
 bot.register_next_step_handler(message, ot3)
 
@@ -113,7 +166,8 @@ if (message.text.lower() == 'один дома') :
 bot.send_message(message.chat.id, 'Верно')
 else:
 bot.send_message(message.chat.id, 'Неверно. Правильный ответ: Один дома')
-text = "Далее: \U0001F525 \U0001F480 \U0001F3CD"
+bot.send_message(message.chat.id, 'Далее: ')
+text = "\U0001F525 \U0001F480 \U0001F3CD"
 bot.send_message(message.chat.id, text=text)
 bot.register_next_step_handler(message, ot2)
 
@@ -122,24 +176,70 @@ bot.send_message(message.chat.id, text=text)
 bot.register_next_step_handler(message, ot1)
 
 if message.text == 'мультфильмы':
+def ot15(message):
+if (message.text.lower() == 'покемон') or (message.text.lower() == 'покемоны'):
+bot.send_message(message.chat.id, 'Верно')
+else:
+bot.send_message(message.chat.id, 'Неверно. Правильный ответ: Покемоны')
+bot.send_message(message.chat.id, 'Игра пройдена!')
+
+def ot14(message):
+if (message.text.lower() == 'тайна коко'):
+bot.send_message(message.chat.id, 'Верно')
+else:
+bot.send_message(message.chat.id, 'Неверно. Правильный ответ: Тайна Коко')
+bot.send_message(message.chat.id, 'Далее: ')
+text = "\U0001F52E \U0001F38A \U0001F431 \U000026A1"
+bot.send_message(message.chat.id, text=text)
+bot.register_next_step_handler(message, ot15)
+
+def ot13(message):
+if (message.text.lower() == 'красавица и чудовище'):
+bot.send_message(message.chat.id, 'Верно')
+else:
+bot.send_message(message.chat.id, 'Неверно. Правильный ответ: Красавица и чудовище')
+bot.send_message(message.chat.id, 'Далее: ')
+text = "\U0001F9D1 \U0001FA95 \U0001FA98 \U0001FA87 \U0001F480"
+bot.send_message(message.chat.id, text=text)
+bot.register_next_step_handler(message, ot14)
+
+def ot12(message):
+if (message.text.lower() == 'рататуй'):
+bot.send_message(message.chat.id, 'Верно')
+else:
+bot.send_message(message.chat.id, 'Неверно. Правильный ответ: Рататуй')
+bot.send_message(message.chat.id, 'Далее: ')
+text = "\U0001F479 \U0001F339 \U0001F478"
+bot.send_message(message.chat.id, text=text)
+bot.register_next_step_handler(message, ot13)
+
+def ot11(message):
+if (message.text.lower() == '101 далматинец') or (message.text.lower() == 'сто один далматинец'):
+bot.send_message(message.chat.id, 'Верно')
+else:
+bot.send_message(message.chat.id, 'Неверно. Правильный ответ: 101 далматинец')
+bot.send_message(message.chat.id, 'Далее: ')
+text = "\U0001F400 \U0001F373"
+bot.send_message(message.chat.id, text=text)
+bot.register_next_step_handler(message, ot12)
+
 def ot10(message):
 if (message.text.lower() == 'том и джери') or (message.text.lower() == 'том и джерри') :
 bot.send_message(message.chat.id, 'Верно')
 else:
 bot.send_message(message.chat.id, 'Неверно. Правильный ответ: Том и Джерри')
-
-markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-i1 = types.KeyboardButton('фильмы')
-i2 = types.KeyboardButton('сериалы')
-markup.add(i1, i2)
-bot.send_message(message.chat.id, 'Хотите пройти другие разделы?', reply_markup=markup)
+bot.send_message(message.chat.id, 'Далее: ')
+text = "\U0001F436 \U00002B1B \U00002B1C"
+bot.send_message(message.chat.id, text=text)
+bot.register_next_step_handler(message, ot11)
 
 def ot9(message):
 if (message.text.lower() == 'труп невесты'):
 bot.send_message(message.chat.id, 'Верно')
 else:
 bot.send_message(message.chat.id, 'Неверно. Правильный ответ: Труп невесты')
-text = "Далее:\U0001F63C \U0001F19A \U0001F42D"
+bot.send_message(message.chat.id, 'Далее: ')
+text = "\U0001F63C \U0001F19A \U0001F42D"
 bot.send_message(message.chat.id, text=text)
 bot.register_next_step_handler(message, ot10)
 
@@ -148,7 +248,8 @@ if (message.text.lower() == 'зверополис'):
 bot.send_message(message.chat.id, 'Верно')
 else:
 bot.send_message(message.chat.id, 'Неверно. Правильный ответ: Зверополис')
-text = "Далее:\U0001F470 \U0001F480"
+bot.send_message(message.chat.id, 'Далее: ')
+text = "\U0001F470 \U0001F480"
 bot.send_message(message.chat.id, text=text)
 bot.register_next_step_handler(message, ot9)
 
@@ -157,16 +258,18 @@ if (message.text.lower() == 'наруто'):
 bot.send_message(message.chat.id, 'Верно')
 else:
 bot.send_message(message.chat.id, 'Неверно. Правильный ответ: Наруто')
-text = "Далее:\U0001F98A \U0001F454 \U0001F430 \U0001F693 "
+bot.send_message(message.chat.id, 'Далее: ')
+text = "\U0001F98A \U0001F454 \U0001F430 \U0001F693 "
 bot.send_message(message.chat.id, text=text)
 bot.register_next_step_handler(message, ot8)
 
 def ot6(message):
-if (message.text.lower() == 'как приручить дракона'):
+if (message.text.lower() == 'приручить дракона') or (message.text.lower() == 'как приручить дракона'):
 bot.send_message(message.chat.id, 'Верно')
 else:
 bot.send_message(message.chat.id, 'Неверно. Правильный ответ: Как приручить даркона')
-text = "Далее:\U0001F343 \U0001F365 \U0001F35C"
+bot.send_message(message.chat.id, 'Далее: ')
+text = "\U0001F343 \U0001F365 \U0001F35C"
 bot.send_message(message.chat.id, text=text)
 bot.register_next_step_handler(message, ot7)
 
@@ -175,17 +278,18 @@ if (message.text.lower() == 'кот в сапогах'):
 bot.send_message(message.chat.id, 'Верно')
 else:
 bot.send_message(message.chat.id, 'Неверно. Правильный ответ: Кот в сапогах')
-text = "Далее:\U0001F9D1 \U0001F44B \U0001F432"
+bot.send_message(message.chat.id, 'Далее: ')
+text = "\U0001F9D1 \U0001F44B \U0001F432"
 bot.send_message(message.chat.id, text=text)
 bot.register_next_step_handler(message, ot6)
 
 def ot4(message):
-
 if (message.text.lower() == 'король лев') :
 bot.send_message(message.chat.id, 'Верно')
 else:
 bot.send_message(message.chat.id, 'Неверно. Правильный ответ: Король Лев')
-text = "Далее:\U0001F431 \U0001F462"
+bot.send_message(message.chat.id, 'Далее: ')
+text = "\U0001F431 \U0001F462"
 bot.send_message(message.chat.id, text=text)
 bot.register_next_step_handler(message, ot5)
 
@@ -194,25 +298,30 @@ if (message.text.lower() == 'унесенная призраками') or (messa
 bot.send_message(message.chat.id, 'Верно')
 else:
 bot.send_message(message.chat.id, 'Неверно. Правильный ответ: Унесенная призраками ')
-text = "Далее:\U0001F981 \U0001F451"
+bot.send_message(message.chat.id, 'Далее: ')
+text = "\U0001F981 \U0001F451"
 bot.send_message(message.chat.id, text=text)
 bot.register_next_step_handler(message, ot4)
 
 def ot2(message):
-if (message.text.lower() == 'вверх') or (message.text.lower() == 'up'):
+if (message.text.lower() == 'вверх') or
+(message.text.lower() == 'up'):
 bot.send_message(message.chat.id, 'Верно')
 else :
 bot.send_message(message.chat.id, 'Неверно. Правильный ответ: Вверх')
-text = "Далее:\U0001F467 \U0001F4A8 \U0001F47B \U0001F46B \U00002194 \U0001F437"
+bot.send_message(message.chat.id, 'Далее: ')
+text = "\U0001F467 \U0001F4A8 \U0001F47B \U0001F46B \U00002194 \U0001F437"
 bot.send_message(message.chat.id, text=text)
 bot.register_next_step_handler(message, ot3)
+
 def ot1(message):
 txt = message.text
 if (message.text.lower() == 'скубиду') or (message.text.lower() == 'скуби-ду') or (message.text.lower() == 'скуби ду'):
 bot.send_message(message.chat.id, 'Верно')
 else:
 bot.send_message(message.chat.id, 'Неверно. Правильный ответ: Скуби-ду ')
-text = "Далее:\U00002B06 \U0001F3DA \U0001F388 "
+bot.send_message(message.chat.id, 'Далее: ')
+text = "\U00002B06 \U0001F3DA \U0001F388 "
 bot.send_message(message.chat.id, text=text)
 bot.register_next_step_handler(message, ot2)
 
@@ -220,13 +329,13 @@ text = "\U0001F436 \U0001F47B \U0001F631"
 bot.send_message(message.chat.id, text=text)
 bot.register_next_step_handler(message, ot1)
 
-if message.text == 'сериалы':
-bot.send_message(message.chat.id, 'пока еще нет')
 
-@bot.message_handler()
+@bot.message_handler( )
 def priv(message):
 if (message.text.lower() == 'здравствуй') or (message.text.lower() == 'здравствуйте') or (message.text.lower() == 'привет') :
-bot.send_message(message.chat.id, f'Привет, {message.from_user.first_name}')
+bot.send_message(message.chat.id, f'Здравствуйте, {message.from_user.first_name}')
+elif (message.text.lower() == 'пока') or (message.text.lower() == 'досвидания') or (message.text.lower() == 'до свидания'):
+bot.send_message(message.chat.id, f'До свидания, {message.from_user.first_name}. Спасибо за внимание!')
 else :
 bot.send_message(message.chat.id, 'Простите, я вас не понимаю. Напишите /help')
 
